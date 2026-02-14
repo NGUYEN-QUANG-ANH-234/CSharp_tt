e66811b36b579cc767fc26c876abbe336ffea1cc
nguyenquanganh <lidehap@gmail.com>
2026-02-14 18:33:30 +0700

    Tái cấu trúc file Program.cs

--- tuan_1/ngay_3_toi_uu/Program.cs
+++ tuan_1/ngay_3_toi_uu/Program.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace ngay_3_toi_uu
     
@@ -16,96 +17,79 @@ namespace ngay_3_toi_uu
     {
         static async Task Main(string[] args)
         {
+            // --- CẤU HÌNH HỆ THỐNG ---
             Console.OutputEncoding = Encoding.UTF8;
+            const int MaxLimit = 2_000_000_000;
+            const int MaxQuantityWord = 100;
+            const long MaxFileSizeMb = 51_200;
 
+            // --- NHẬP LIỆU VÀ KHỞI TẠO FILE ---
             string baseDir = AppContext.BaseDirectory;
             string filePath = Path.Combine(baseDir, "DummyLogFile.txt");
 
             Console.Write("Nhập dung lượng file (MB): ");
-            string input = Console.ReadLine();
+            var sizeInMb = InputHelper.GetValidLong($"Vui lòng nhập số dòng (1 - {MaxFileSizeMb:N0}): ", 1, MaxFileSizeMb);
 
-            // Sử dụng TryParse để kiểm tra logic mà không ném Exception (Hiệu năng cao)
-            if (!long.TryParse(input, out long sizeInMb) || sizeInMb <= 0)
-            {
-                // Guard Clause: Xử lý sai lệch sớm
-                Console.WriteLine("Lỗi: Vui lòng nhập một số nguyên dương hợp lệ.");
-                return;
-            }
-
-            // Sau khi đã an toàn, mới thực hiện dịch bit
             long targetSizeBytes = sizeInMb << 20;
 
-
             FileInfo file = new FileInfo(filePath);
-            if (file.Exists && file.Length > (targetSizeBytes))
+            if (!file.Exists || file.Length < targetSizeBytes)
             {
-                Console.WriteLine($"Da ton tai File xap xi ({sizeInMb} MB). Bo qua buoc tao file.");
-            }
-            else
-            {
-                Console.WriteLine("File chua co hoac bi rong. Bat dau tao file moi...");
-                GenerateLogFile.GenerateDummyLogFile(filePath, targetSizeBytes);
+                Console.WriteLine("[ERROR] File chưa tồn tại. Bắt đầu tạo file mới...");
+                GenerateLogFile.Generate(filePath, targetSizeBytes);
             }
 
-            Console.WriteLine("Nhập số dòng trong Records muốn xử lý: ");
-            var inputLimit = Console.ReadLine();
-            const int MaxLimit = 2_000_000_000;
+            Console.WriteLine($"[OK] Đã tồn tại file xấp xỉ ({sizeInMb} MB), bỏ qua bước tạo file.");
 
-            if (!int.TryParse(inputLimit, out int lineLimit) || lineLimit <= 0 || lineLimit > MaxLimit)
-            {
-                Console.WriteLine($"Lỗi: Vui lòng nhập số nguyên dương từ 1 đến {MaxLimit:N0}.");
-                return;
-            }
+            // --- CẤU HÌNH THÔNG SỐ XỬ LÝ ---
+            var lineQuantity = InputHelper.GetValidInt($"Vui lòng nhập số dòng (1 - {MaxLimit:N0}): ", 1, MaxLimit);
+            Console.WriteLine($"[OK] Hệ thống sẽ xử lý {lineQuantity:N0} dòng.");
 
-            int recordLimit = lineLimit;
+            var wordQuantity = InputHelper.GetValidInt($"Nhập số lượng từ xuất hiện nhiều nhất (1 - {MaxQuantityWord:N0}): ", 1, MaxQuantityWord);
+            Console.WriteLine($"[OK] Hệ thống sẽ xử lý {wordQuantity:N0} từ.");
 
-            // 2. Sync I/O 
-            Console.WriteLine($"--- Đang load {recordLimit:N0} records (Sync I/O) ---");
+            // --- LOAD FILE ---
+            int recordLimit = lineQuantity;
             Stopwatch sw = Stopwatch.StartNew();
 
+            // Sync I/O 
+            Console.WriteLine($"--- Đang load {recordLimit:N0} records (Sync I/O) ---");
             var syncProcessor = new SyncLoadRecords();
             var syncRecords = syncProcessor.LoadRecords(filePath, recordLimit);
             sw.Stop();
-            Console.WriteLine($"Load Sync hoàn tất: {sw.ElapsedMilliseconds} ms\n");
+            Console.WriteLine($"[OK] Load Sync hoàn tất: {sw.ElapsedMilliseconds} ms\n");
 
             sw.Restart();
 
-            // 3. Async I/O 
+            // Async I/O 
             Console.WriteLine($"--- Đang load {recordLimit:N0} records (Async I/O) ---");
-
             var asyncProcessor = new AsyncLoadRecords();
             var asyncRecords = await asyncProcessor.LoadRecords(filePath, recordLimit);
             sw.Stop();
-            Console.WriteLine($"Load Async hoàn tất: {sw.ElapsedMilliseconds} ms\n");        
-
-            Console.Write("Nhập số lượng Top words: ");
-            if (!int.TryParse(Console.ReadLine(), out int numberOfTopWords)) return;
-
-            sw.Stop();  
+            Console.WriteLine($"[OK] Load Async hoàn tất: {sw.ElapsedMilliseconds} ms\n");        
 
             sw.Restart();
 
-            // 4. Benchmark Tuần tự
+            // --- ĐẾM TỪ ---
+            // Xử lý tuần tự
             Console.WriteLine("\n--- Đang xử lý TUẦN TỰ ---");
             var sequential = new SequentialWordsCounter();
             sw.Restart();
             sequential.Execute(asyncRecords);
-            var seqResults = sequential.GetTopWords(numberOfTopWords);
+            var seqResults = sequential.GetTopWords(wordQuantity);
 
             sw.Stop();
             AnalyzeLog.PrintResults(seqResults, sw.ElapsedMilliseconds, sequential.GetTotalWordsCount());
 
-            // 5. Benchmark PLINQ (Song song)
+            // Xử lý song song
             Console.WriteLine("\n--- Đang xử lý SONG SONG ---");
             var parallel = new ParallelWordsCounter();
             sw.Restart();
             parallel.Execute(asyncRecords);
-            var parResults = parallel.GetTopWords(numberOfTopWords);
+            var parResults = parallel.GetTopWords(wordQuantity);
 
             sw.Stop();
             AnalyzeLog.PrintResults(parResults, sw.ElapsedMilliseconds, parallel.GetTotalWordsCount());
         }
-
-
     }
 }
