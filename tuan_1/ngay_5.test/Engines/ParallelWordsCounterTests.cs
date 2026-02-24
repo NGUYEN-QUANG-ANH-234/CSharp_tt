0fa498967d93eb2a61890b9f3dc8c5ebafd52b5e
nguyenquanganh <lidehap@gmail.com>
2026-02-24 22:45:22 +0700

    Bo sung UnitTest ngay 5

---
+++ tuan_1/ngay_5.test/Engines/ParallelWordsCounterTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Logging;
+using ngay_5.Engines;
+using Serilog;
+
+namespace ngay_5.test.Engines
+{
+    public class ParallelWordsCounterTests
+    {
+        // ParallelWordsCounter_Execute_Test
+
+        // Happy Path & Edge Cases
+        [Fact]
+        public void Execute_LinesOfLogType_ReturnDictionaryOfLogType()
+        {
+            using var factory = LoggerFactory.Create(builder => builder.AddSerilog());
+            var logger = factory.CreateLogger<ParallelWordsCounter>();
+
+            // Arrange
+            var counter = new ParallelWordsCounter();
+
+            var input = new List<string>
+            {
+                "18:20:40 INFO: User logged in successfully",
+                "INFO: User logged in successfully",
+                "ABC WARN: Deprecated API endpoint used",
+            };
+
+            var expected = new Dictionary<string, long>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "INFO", 2 },
+                { "WARN", 1 }
+            };
+
+            // Act
+            counter.Execute(logger, input);
+            var result = counter.GetResult();
+
+            // Assert
+            Assert.Equal(result, expected);
+        }
+
+        [Fact]
+        // Negative Path
+        public void Execute_InvalidLinesOfLogType_ReturnsNullDictionary()
+        {
+            using var factory = LoggerFactory.Create(builder => builder.AddSerilog());
+            var logger = factory.CreateLogger<ParallelWordsCounter>();
+
+            // Arrange
+            var counter = new ParallelWordsCounter();
+
+            var input = new List<string>
+            {
+                "",
+                "HELLO: This is not a log type",
+                "12345: Just numbers",
+                "info: Case sensitive check"
+            };
+
+            var expectedCount = 0;
+
+            counter.Execute(logger ,input);
+            var result = counter.GetResult();
+
+            // Assert
+            Assert.Equal(result.Count, expectedCount);
+        }
+    }
+}
---
+++ tuan_1/ngay_5.test/Utilites/WordsUtilityTests.cs
@@ -0,0 +1,34 @@
+﻿using ngay_5.Engines;
+using ngay_5.Utilities;
+using Xunit;
+
+namespace ngay_5.test.Utilites
+{
+    public class WordsUtilityTests
+    {
+        // WordsUtility_Extract_Test
+
+        // Happy Path & Edge Cases
+        [Theory]
+        [InlineData("INFO: User logged in successfully", "INFO")]
+        [InlineData("ABC WARN: Deprecated API endpoint used", "WARN")]
+        [InlineData("18:20:40 INFO: User logged in successfully", "INFO")]
+        public void Extract_LogLine_ReturnsExpectedLogTypes(string input, string expected)
+        {
+            string result = WordsUtility.Extract(input);
+            Assert.Equal(expected, result);
+        }
+
+        // Negative Path
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("HELLO: This is not a log type", "")]
+        [InlineData("12345: Just numbers", "")]
+        [InlineData("info: Case sensitive check", "")]
+        public void Extract_InvalidLogLine_ReturnsEmptyLogTypes(string input, string expected)
+        {
+            string result = WordsUtility.Extract(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
